994d0c0f0bb2aa42d0e20ca6f80503e7298efcc0
jack456054 <jack456054@hotmail.com>
2019-07-30 17:36:23 +0800

    Stop gninnipS My sdroW

---
+++ Stop_gninnipS_My_sdroW.py
@@ -0,0 +1,12 @@
+def spin_words(sentence):
+    # result = ''.join(list(reversed(sentence)))
+    words_list = sentence.split()
+    for index, word in enumerate(words_list):
+        if len(word) >= 5:
+            words_list[index] = ''.join(list(reversed(words_list[index])))
+
+    return ' '.join(words_list)
+
+
+if __name__ == '__main__':
+    print(spin_words("I an so handsome"))
